984c99529a215b6ce0446923ca8ae7e2cf5a40bd
tttol <tkhstol.929@gmail.com>
2026-01-07 09:29:08 +0900

    update agent-memory and sh

--- copy_from_claude.sh
+++ copy_from_claude.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -maxdepth 1 ! -name 'copy_from_claude.sh' ! -name '.' -exec rm -rf {} +
+find . -maxdepth 1 ! -name 'copy_from_claude.sh' ! -name '.' ! -name '.git' -exec rm -rf {} +
 cp -r ~/.claude/skills/ ./
